3d13658deeeb41cb1e1aa4b1a1e7b37c8518381f
David G. Moore, Jr <david@dgmjr.io>
2023-07-29 19:06:00 -0400

    adding .gitignore

---
+++ src/EnumerationGenerator.cs
@@ -0,0 +1,12 @@
+namespace Dgmjr.Enumerations.CodeGenerator;
+using Microsoft.CSharp;
+using Microsoft.CodeAnalysis;
+
+[Generator]
+public class EnumerationGenerator : IIncrementalGenerator
+{
+    public void Initilalize(IncrementalGeneratorInitializationContext context)
+    {
+
+    }
+}
