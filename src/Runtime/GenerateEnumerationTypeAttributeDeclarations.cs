60fd30f00fc988df796f509535b408cf8265f92e
David G. Moore, Jr <david@dgmjr.io>
2023-10-04 17:03:50 -0400

    try'n'a get this shit to f'ing work

--- src/Runtime/GenerateEnumerationTypeAttributeDeclarations.cs
+++ src/Runtime/GenerateEnumerationTypeAttributeDeclarations.cs
@@ -1,7 +1,7 @@
 namespace System;
 
 [AttributeUsage(AttributeTargets.Class | AttributeTargets.Struct | AttributeTargets.Enum)]
-public class GenerateEnumerationTypeAttribute(
+public abstract class GenerateEnumerationTypeAttribute(
     string? typeName = default,
     string? @namespace = default
 ) : Attribute
