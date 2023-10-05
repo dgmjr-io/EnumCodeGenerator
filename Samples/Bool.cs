c77fd09c39bce24b5e4b777596c5120dfc5e42da
David G. Moore, Jr <david@dgmjr.io>
2023-10-05 01:04:34 -0400

    The changes made in this commit include: - Added Bool.cs - Modified Dgmjr.Enumerations.CodeGenerator.Samples.csproj to include a SourceGeneratorPackageReference for Dgmjr.Enumerations.CodeGenerator - Deleted ExitCodeEnum.cs - Modified HttpMethodsEnum.cs to change the namespace from Dgmjr.Enumerations.CodeGenerator.Samples.Enums to System.Net.Http.Enums

---
+++ Samples/Bool.cs
@@ -0,0 +1,20 @@
+/*
+ * Bool.cs
+ *
+ *   Created: 2023-10-05-12:34:29
+ *   Modified: 2023-10-05-12:34:29
+ *
+ *   Author: David G. Moore, Jr. <david@dgmjr.io>
+ *
+ *   Copyright © 2022 - 2023 David G. Moore, Jr., All Rights Reserved
+ *      License: MIT (https://opensource.org/licenses/MIT)
+ */
+
+namespace System.Enums;
+
+[GenerateEnumerationRecordStruct("Bool", "System")]
+public enum Bool
+{
+    True = 1,
+    False = 0
+}
